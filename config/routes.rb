6e64c29a7a09141c0847fc1b35197b7f1e0ff115
melissa160 <melissa160@gmail.com>
2017-04-27 12:47:16 -0500

    estableciendo rutas

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,8 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-
-  get 'posts/show'
-
-  get 'posts/create'
-
-  get 'posts/update'
-
-  get 'posts/new'
-
-  get 'posts/destroy'
-
-  get 'posts/edit'
-
   devise_for :users
+  resources :posts do
+    resources :comments, only: [:create, :destroy, :edit]
+  end
+  root "posts#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
